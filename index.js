43c13c082c538dd9e133623eb9de8f5094d1d1aa
Safoun <safoun.official@gmail.com>
2023-05-02 17:19:41 +0600

    initializing repository , accidentally added node packages

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+// const categories = require("./data/categories.json");
+// const news = require("./data/news.json");
+
+const cors = require('cors');
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send('Dragon is running!!!');
+});
+
+// app.get("/categories", (req, res) => {
+//     res.send(categories);
+// });
+
+// app.get("/news", (req, res) => {
+//     res.send(news);
+// });
+
+// app.get("/news/:ID", (req, res) => {
+//     const ID = req.params.ID;
+//     const selectedNews = news.find(n => n._id == ID);
+//     res.send(selectedNews)
+// });
+
+// app.get("/categories/:ID", (req, res) => {
+//     const ID = req.params.ID;
+//     if (ID === "0") {
+//         res.send(news);
+//     }
+//     else {
+//         const categoryNews = news.filter(n => n.category_id == ID);
+//         res.send(categoryNews);
+//     }
+// });
+
+app.listen(port, () => {
+    console.log(`dragon API is running on port ${port}`);
+})
